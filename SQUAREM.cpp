6729663009e9560dafad5c5823ff49d493620ad1
daichaoxing <daichaoxing@gmail.com>
2015-01-21 01:38:43 -0600

    squarem1 updated

--- SQUAREM.cpp
+++ SQUAREM.cpp
@@ -3,9 +3,14 @@
 //2. additional input to each of the functions
 //3. Test squarem1 on R and compare with the result by original package
 //4. If squarem1 passed,then implement the other 3+1 functions
-
 //5. Search for cpp equivalent 	coef <- try(solve(qr(crossprod(U), LAPACK=TRUE, tol=1.e-14), rep(1,K+1)), silent=TRUE)
 //6. Search for cpp equivalent 	coef <- try(solve(qr(U[,-(K+1)], LAPACK=TRUE, tol=1.e-14), -U[,K+1]), silent=TRUE)
+//7. Allow user to specify the control variables
+
+//Increase # of inputs: testingY
+//GITHUB implementation
+//Increase # of functions
+//SEXP pointer?
 
 #include <iostream>
 #include <string>
@@ -26,12 +31,12 @@ const int method=3;//1,2,3 indicates the types of step length to be used in squa
 // K>1 must go with method=4 or 5
 const int mstep=4;
 const int maxiter=1500;
-const bool square=true;
-const bool trace=true;
+//const bool square=true;
+const bool trace=true;//currently set to be true for debugging purpose
 const double stepmin0=1;
 const double stepmax0=1;
-const double kr=1;
-const double objfninc=1;
+//const double kr=1;
+const double objfninc=1;//0 to enforce monotonicity, Inf for non-monotonic scheme, 1 for monotonicity far from solution and allows for non-monotonicity closer to solution
 const double tol=1e-7;
 
 
@@ -83,27 +88,32 @@ List squarem1(NumericVector par,Function fixptfn,Function objfn,NumericVector te
         std::cout<<"Squarem2 should be used if objective function is not available!"<<std::endl;
         return 0;
     }
-    iter=1;p=par;lold=objfn(p,testingY);leval=1;
+    iter=1;p=par;
+    try{lold=objfn(p,testingY);leval=1;}
+    catch(...){
+        std::cout<<"Error in fixptfn function evaluation";
+        return 1;
+    }
     loldcpp=lold;
     lnew=lold;
-    
-    if(trace){std::cout<<"Objective fn: "<<lold<<std::endl;}
+    if(trace){std::cout<<"Objective fn: "<<loldcpp[0]<<std::endl;}
     feval=0;conv=true;
+    
     
     while(feval<maxiter){
         //Step 1
         extrap = true;
-        pcpp=p;
         try{p1=fixptfn(p,testingY);feval++;}
         catch(...){
             std::cout<<"Error in fixptfn function evaluation";
             return 1;
         }
+        pcpp=p;
         p1cpp=p1;
-        q1=p1cpp-pcpp;
-        sr2=q1*q1;
+        q1=p1cpp-pcpp;//conversion from SEXP to vector for crossprod
+        sr2=q1;
         sr2_scalar=0;
-        for (int i=0;i<sr2.length();i++){sr2_scalar+=sr2[i];}
+        for (int i=0;i<sr2.length();i++){sr2_scalar+=sr2[i]*sr2[i];}
         if(sqrt(sr2_scalar)<tol){break;}
         
         //Step 2
@@ -123,8 +133,9 @@ List squarem1(NumericVector par,Function fixptfn,Function objfn,NumericVector te
         sv2_scalar=0;
         for (int i=0;i<sv2.length();i++){sv2_scalar+=sv2[i]*sv2[i];}
         srv_scalar=0;
-        for (int i=0;i<q2.length();i++){sr2_scalar+=sv2[i]*q1[i];}
-        
+        for (int i=0;i<q2.length();i++){srv_scalar+=sv2[i]*q1[i];}
+        //std::cout<<"sr2,sv2,srv="<<sr2_scalar<<","<<sv2_scalar<<","<<srv_scalar<<std::endl;//debugging
+
         //Step 3 Proposing new value
         switch (method){
             case 1: alpha= -srv_scalar/sv2_scalar;
@@ -136,6 +147,7 @@ List squarem1(NumericVector par,Function fixptfn,Function objfn,NumericVector te
         }
         
         alpha=std::max(stepmin,std::min(stepmax,alpha));
+        //std::cout<<"alpha="<<alpha<<std::endl;//debugging
         pnew = pcpp + 2.0*alpha*q1 + alpha*alpha*(q2-q1);
         
         //Step 4 stabilization
@@ -155,10 +167,11 @@ List squarem1(NumericVector par,Function fixptfn,Function objfn,NumericVector te
                 if(alpha==stepmax){stepmax=mstep*stepmax;}
                 if(stepmin<0 && alpha==stepmin){stepmin=mstep*stepmin;}
                 p=pnew;
-                //DCX if(!std::isnan(lnew)){lold=lnew;}
-                if(trace){std::cout<<"Objective fn: "<<lnew<<"  Extrapolation: "<<extrap<<"  Steplength: "<<alpha<<"\n"<<std::endl;}
+                lnewcpp=lnew;
+                if(!std::isnan(lnewcpp[0])){lold=lnew;}
+                if(trace){std::cout<<"Objective fn: "<<lnewcpp[0]<<"  Extrapolation: "<<extrap<<"  Steplength: "<<alpha<<"\n"<<std::endl;}
                 iter++;
-                continue;
+                continue;//next round in while loop
             }
             
             if (isfinite(objfninc)){
@@ -176,6 +189,20 @@ List squarem1(NumericVector par,Function fixptfn,Function objfn,NumericVector te
                     alpha=1;
                     extrap=false;
                 }
+            }else{lnew=lold;}
+            lnewcpp=lnew;
+            if (lnewcpp[0]>loldcpp[0]+objfninc) {
+                pnew=p2;
+                try{lnew=objfn(pnew,testingY);leval++;}
+                catch(...){
+                    std::cout<<"Error in objfn function evaluation";
+                    return 1;
+                }
+                if(alpha==stepmax){
+                    stepmax=std::max(stepmax0,stepmax/mstep);
+                }
+                alpha=1;
+                extrap=false;
             }
         }else{//same as above, when stablization is not performed.
             if (isfinite(objfninc)){
@@ -193,6 +220,20 @@ List squarem1(NumericVector par,Function fixptfn,Function objfn,NumericVector te
                     alpha=1;
                     extrap=false;
                 }
+            }else{lnew=lold;}
+            lnewcpp=lnew;
+            if (lnewcpp[0]>loldcpp[0]+objfninc) {
+                pnew=p2;
+                try{lnew=objfn(pnew,testingY);leval++;}
+                catch(...){
+                    std::cout<<"Error in objfn function evaluation";
+                    return 1;
+                }
+                if(alpha==stepmax){
+                    stepmax=std::max(stepmax0,stepmax/mstep);
+                }
+                alpha=1;
+                extrap=false;
             }
         }
         
@@ -201,8 +242,10 @@ List squarem1(NumericVector par,Function fixptfn,Function objfn,NumericVector te
         if(stepmin<0 && alpha==stepmin){stepmin=mstep*stepmin;}
         
         p=pnew;
-        //DCX if(!std::isnan(lnew)){lold=lnew;}
-        if(trace){std::cout<<"Objective fn: "<<lnew<<"  Extrapolation: "<<extrap<<"  Steplength: "<<alpha<<"\n"<<std::endl;}
+        lnewcpp=lnew;
+        if(!std::isnan(lnewcpp[0])){lold=lnew;}
+        loldcpp=lold;
+        if(trace){std::cout<<"Objective fn: "<<lnewcpp[0]<<"  Extrapolation: "<<extrap<<"  Steplength: "<<alpha<<"\n"<<std::endl;}
         
         iter++;
     }
@@ -226,8 +269,6 @@ List squarem2(NumericVector par,Function fixptfn){
     List sqobj;
     return sqobj;
 }
-    
-    
 List cyclem1(NumericVector par,Function fixptfn,Function objfn){
     List sqobj;
     return sqobj;
@@ -237,7 +278,12 @@ List cyclem2(NumericVector par,Function fixptfn,Function objfn){
     return sqobj;
 }
 
+List squaremtest(NumericVector par,Function fixptfn,Function objfn){
+    List sqobj;
+    return sqobj;
+}
 
+//main() used for debugging in Xcode
 int main(){
     std::cout<<"Hello "<<tol<<std::endl;
     double tol1=tol*10000000;
